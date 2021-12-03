8c0e8171d36acbf625138804f7847c6144e99c19
Sahar Halim <saharhalim151@gmail.com>
2021-12-02 21:47:41 -0500

    Update breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,19 +6,11 @@ const fetchBreedDescription = function(breedName, callback) {
       return callback(error,null); // Print the error if one occurred
     } 
   
-    // if (response.statusCode !== 200) {
-    // return callback('statusCode:'+response.statusCode, null); // Print the response status code if a response was received
-    // }
-     console.log('body:', body); // Print the HTML for the Google homepage.
-  
     const data = JSON.parse(body);
     // const breedDescription = (data[0]["description"]);
-    // if (!data.length) {
-    //  return callback('Cat breed is not found ', null);
-    // } 
-    // console.log(breedDescription);
+    
     console.log(data);
-    //   return returnFunc(null, breedDescription);
+    
   });
 };
 
